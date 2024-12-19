d9e0bddc9e6374a6fc3e8793548aa0721b5eaabe
pauloh-fm <phfarmar@gmail.com>
2024-12-19 20:45:28 -0300

    fix : remove main function

--- area-trapezio.c
+++ area-trapezio.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 // Função para calcular a área do trapézio
-float calcularAreaTrapezio() {
+float calcularAreaTrapezio(void) {
     float baseMaior, baseMenor, altura;
     // Entrada de dados
     printf("Digite o valor da base maior: ");
@@ -13,15 +13,4 @@ float calcularAreaTrapezio() {
     printf("Digite o valor da altura: ");
     scanf("%f", &altura);
     return ((baseMaior + baseMenor) * altura) / 2;
-}
-
-int main() {
-    float area;
-    // Chamada da função para calcular a área
-    area = calcularAreaTrapezio();
-
-    // Exibição do resultado
-    printf("A área do trapézio é: %.2f\n", area);
-
-    return 0;
 }
